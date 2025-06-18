b28e9081fea323dc4375ac379d3fd96cce733a09
pipikanha <lcontatoandrade@gmail.com>
2025-06-18 14:49:18 -0300

    Prototype_commit
    my first movement prototype

---
+++ Assets/Scipts/MISC/Player.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Dynamic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+     public Rigidbody2D myRigidbody;
+     public Vector2 velocity;
+    public float speed;
+
+     private void FixedUpdate()
+     {
+         if (Input.GetKey(KeyCode.LeftArrow))
+         {
+            // myRigidbody.MovePosition(myRigidbody.position - velocity * Time.deltaTime);
+    
+            myRigidbody.velocity = new Vector2(-speed, velocity.y);
+         }
+         else if (Input.GetKey(KeyCode.RightArrow))
+         {
+            //myRigidbody.MovePosition(myRigidbody.position + velocity * Time.deltaTime);
+            
+            myRigidbody.velocity = new Vector2(speed, velocity.y);
+        }
+     }
+
+
+   
+
+    
+}
+
